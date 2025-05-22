472618da4540576e94a01d25ad3b8a48e18228ff
jaspal2 <singhh.jaspal27@gmail.com>
2025-05-22 16:00:40 +1000

    adding sg group

---
+++ security-group/main.tf
@@ -0,0 +1,12 @@
+
+
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+}
