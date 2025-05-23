c7501adb251dedd08d22ddd2d65588290448be5b
jaspal2 <singhh.jaspal27@gmail.com>
2025-05-23 14:47:57 +1000

    removed asg

--- main.tf
+++ main.tf
@@ -7,7 +7,3 @@ module "security-group" {
   vpc_id = module.vpc.vpc_id
 }
 
-module "asg" {
-  source = "./asg"
-  vpc_zone_identifier = module.vpc.public_subnets
-}
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../terraform-aws-autoscaling-master/"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
