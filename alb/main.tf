59ea71aa878f66cc1f124df777940fc7f4bd8f2d
jaspal2 <singhh.jaspal27@gmail.com>
2025-05-29 13:43:22 +1000

    adding target group

---
+++ alb/main.tf
@@ -0,0 +1,54 @@
+resource "aws_lb_target_group" "test_target_group" {
+  name     = "test_taget_group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "instance"
+}
+
+/*module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "my-alb"
+  vpc_id  = var.vpc_id
+  subnets = var.public_subnets
+  create_security_group = false
+  security_groups = var.security_group
+  ip_address_type = "ipv4"
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}*/
--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ module "asg" {
   vpc_zone_identifier = module.vpc.public_subnets
   security_group_id   = module.security-group.security_group_id
 }
+
+module "alb" {
+  source = "./alb"
+  vpc_id = module.vpc.vpc_id
+  security_group = module.security-group.security_group_id
+  public_subnets = [module.vpc.public_subnets]
+}
