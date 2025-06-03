f34df85292d0b25dc784d7d09ee886fe188ca06d
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-03 13:57:57 +1000

    Adding random

--- alb/main.tf
+++ alb/main.tf
@@ -27,6 +27,7 @@ module "alb" {
   create_security_group = false
   security_groups = [var.security_group]
   ip_address_type = "ipv4"
+  enable_deletion_protection = true
 }
 
 
@@ -38,5 +39,29 @@ resource "aws_lb_listener" "front_end" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.test-target-group.arn
+  }
+}
+
+resource "random_string" "string_2" {
+  length = 8
+
+}
+
+
+resource "aws_security_group" "web-sg" {
+
+  name = randon_string.string_2.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- security-group/main.tf
+++ security-group/main.tf
@@ -7,7 +7,7 @@ module "terraform_service_sg" {
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = var.vpc_id
 
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
   ingress_rules            = ["https-443-tcp",  "ssh-tcp", "http-80-tcp"]
   egress_rules              = ["all-all"]
 }
