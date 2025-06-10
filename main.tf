a48f99dc0df19bceb428508905da61b1e7fd4d57
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-10 10:21:20 +1000

    changed instance number

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "security-group" {
   source = "./security-group"
   vpc_id = module.vpc.vpc_id
 }
-/*
+
 module "asg" {
   source = "./asg"
   vpc_zone_identifier = module.vpc.public_subnets
@@ -20,20 +20,21 @@ module "alb" {
   public_subnets = module.vpc.public_subnets
 }
 
-/*resource "aws_lb_target_group" "test-target-group" {
+resource "aws_lb_target_group" "test-target-group" {
   name     = "test-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
-}*/
+}
 
 # Create a new load balancer attachment
-/*resource "aws_autoscaling_attachment" "example" {
+resource "aws_autoscaling_attachment" "example" {
   autoscaling_group_name = module.asg.autoscaling_group_id
    lb_target_group_arn    = module.alb.target_id
 }
 
-*/
+
+/*
 data "aws_ami" "terraform_ami" {
   most_recent      = true
   owners           = ["099720109477"]
@@ -66,5 +67,5 @@ resource "aws_instance" "app" {
   #subnet_id = var.public_subnets[count.index % length(var.public_subnets)]
   subnet_id = "10.0.101.0/24"
   security_groups = [module.security-group.security_group_id]
-}
+}*/
 
