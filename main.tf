08af94fad63ac60e2163d6045a6ddb04fb9a14a6
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-10 14:15:15 +1000

    depend on

--- main.tf
+++ main.tf
@@ -3,17 +3,20 @@ module "vpc" {
 }
 
 module "security-group" {
+  depends_on = [module.vpc]
   source = "./security-group"
   vpc_id = module.vpc.vpc_id
 }
 
 module "asg" {
+  depends_on = [module.vpc, module.security-group]
   source = "./asg"
   vpc_zone_identifier = module.vpc.public_subnets
   security_group_id   = module.security-group.security_group_id
 }
 
 module "alb" {
+  depends_on = [module.vpc, module.security-group]
   source = "./alb"
   vpc_id = module.vpc.vpc_id
   security_group = module.security-group.security_group_id
@@ -29,6 +32,7 @@ resource "aws_lb_target_group" "test-target-group" {
 
 # Create a new load balancer attachment
 resource "aws_autoscaling_attachment" "example" {
+  depends_on = [module.alb, module.asg]
   autoscaling_group_name = module.asg.autoscaling_group_id
    lb_target_group_arn    = module.alb.target_id
 }
