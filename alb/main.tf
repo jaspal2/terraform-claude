ae2196d0b4d3cb0d0b480f30047a359a84266e5a
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-11 14:03:30 +1000

    depend on vpc space

--- alb/main.tf
+++ alb/main.tf
@@ -44,17 +44,6 @@ resource "aws_lb_listener" "front_end" {
 }
 
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = module.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.test-target-group.arn
-  }
-}
-
 resource "random_string" "string_2" {
   length = 8
 
