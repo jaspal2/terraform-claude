07b7043530c6a7587565d4fcce7a27a9321c88c7
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-11 16:03:56 +1000

    depend on vpc space

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "security-group" {
   source = "./security-group"
   vpc_id = module.vpc.vpc_id
 }
-/*
+
 module "asg" {
   depends_on = [module.vpc, module.security-group]
   source = "./asg"
@@ -23,15 +23,12 @@ module "alb" {
   public_subnets = module.vpc.public_subnets
 }
 
-
-
 resource "aws_lb_target_group" "test-target-group" {
   name     = "test-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
 }
-
 
 # Create a new load balancer attachment
 resource "aws_autoscaling_attachment" "example" {
@@ -41,7 +38,7 @@ resource "aws_autoscaling_attachment" "example" {
 }
 
 
-*/
+/*
 data "aws_ami" "terraform_ami" {
   most_recent      = true
   owners           = ["099720109477"]
@@ -57,21 +54,9 @@ data "aws_ami" "terraform_ami" {
     values = ["hvm"]
   }
 }
-
+/*
 resource "aws_key_pair" "public_key" {
   key_name   = "terraform-key"
   public_key =  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzUBT9HRDJYhhS6rS1cqlXug/Wnv33UZbQ4UIHombPH jaspal.singh@monash.edu"
 }
-
-resource "aws_instance" "app" {
-  count = 2
-  depends_on = [module.vpc, module.security-group]
-
-  ami           = data.aws_ami.terraform_ami.id
-  instance_type = "t2.micro"
-
-  subnet_id = module.vpc.public_subnets[count.index]
-  #subnet_id = module.vpc.public_subnets[0]
-  security_groups = [module.security-group.security_group_id]
-}
-
+*/
