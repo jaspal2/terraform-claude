f8736b93005b77e2353e94fc56aeac04aedf4834
jaspal2 <singhh.jaspal27@gmail.com>
2025-06-20 13:33:36 +1000

    dynamic vpc

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
   source = "./vpc"
 }
-/*
+
 module "security-group" {
   depends_on = [module.vpc]
   source = "./security-group"
   vpc_id = module.vpc.vpc_id
-}*/
+}
 /*
 module "asg" {
   depends_on = [module.vpc, module.security-group]
@@ -109,7 +109,7 @@ resource "aws_instance" "example" {
 }
 */
 
-/*
+
 data "aws_availability_zones" "available_zone" {
   state = "available"
   filter {
@@ -140,21 +140,25 @@ locals {
   is_qa              = var.environmen  == "qa"
   az_count           =  length(data.aws_availability_zones.available_zone.names)
 
-  instance_count     =  local.is_prod ?  length(data.aws_availability_zones.available_zone.names) : 2
-
+  instance_count     =  local.is_prod ? 3 : (local.is_qa ? 2 : 1)
   vpc_cidr = is_prod ? "10.0.0.0/16" : "10.${local.is_qa ? 1 : 2}.0.0/16"
   public_subnets = [
     for index in range(local.az_count):
             cidrsubnet(local.vpc_cidr, 8 , index+1 )
   ]
 
+  tags = {
+    "name" : "web-server"
+    "env" : var.environmen
+   }
+
 }
 
 
-resource "aws_instance" "example" {
+resource "aws_instance" "webserver" {
   #depends_on = [aws_iam_role.test_role]
 
-
+  count =       local.instance_count * local.az_count
   ami           = data.aws_ami.terraform_ami.id
   instance_type = "t2.micro"
   subnet_id     = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
@@ -163,7 +167,29 @@ resource "aws_instance" "example" {
 
   #iam_instance_profile = aws_iam_instance_profile.test_instance_profile.name
 
-  tags = merge(local.tags)
+  tags = merge(local.tags, {
+    "server" : "web-server"
+  })
 
-}
-*/
+    }
+
+
+resource "aws_instance" "app-server" {
+  #depends_on = [aws_iam_role.test_role]
+
+  count =       local.is_prod ? 2 : 1
+  ami           = data.aws_ami.terraform_ami.id
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
+  security_groups = [module.security-group.security_group_id]
+  associate_public_ip_address = (count.index%2 == 0 ? true : false)
+
+  #iam_instance_profile = aws_iam_instance_profile.test_instance_profile.name
+
+  tags = merge(local.tags, {
+    "server" : "web-server"
+  })
+
+    }
+
+
